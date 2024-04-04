a5db1a71b14607d22b8887576de3d31f592b5703
aarrosier <rosier.alex@icloud.com>
2024-04-04 22:13:40 +0100

    changed object property
    changed object property from author to source.

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ project 1 - A Random Quote Generator
 Array of objects: 
 
 quote:"" // Quote to display
-author:"" // Quote Author
+source:"" // Quote source
 citation:"" // Optional Citation, i.e: Where Quote is from
 year:"" // Optional year of publication
 tag:"" // Optional tag. 
@@ -22,76 +22,76 @@ tag:"" // Optional tag.
 const quotes = [
     {
       quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-      author: "Nelson Mandela",
+      source: "Nelson Mandela",
       citation: "Long walk to freedom",
       year: "1994",
       tags: "<i>(inspirational wisdom)</i>"
     },
     {
       quote: "The way to get started is to quit talking and begin doing.",
-      author: "Walt Disney",
+      source: "Walt Disney",
       tags: "<i>(inspirational wisdom)</i>"
     },
     {
       quote: "Life is what happens when you're busy making other plans.",
-      author: "John Lennon",
+      source: "John Lennon",
       citation: "Beautiful Boy (Darling Boy) - Double Fantasy Album",
       year: "1980"
     },
     {
       quote: "The future belongs to those who believe in the beauty of their dreams.",
-      author: "Eleanor Roosevelt"
+      source: "Eleanor Roosevelt"
     },
     {
       quote: "It is during our darkest moments that we must focus to see the light.",
-      author: "Aristotle"
+      source: "Aristotle"
     },
     {
       quote: "Whoever is happy will make others happy too.",
-      author: "Anne Frank",
+      source: "Anne Frank",
       citation: "The Diary of a young girl",
       year: "1947"
     }, 
     {
       quote: "Do not go where the path may lead, go instead where there is no path and leave a trail.",
-      author: "Ralph Waldo Emerson"
+      source: "Ralph Waldo Emerson"
     },
     {
       quote: "You will face many defeats in life, but never let yourself be defeated. ",
-      author: "Maya Angelou",
+      source: "Maya Angelou",
       tags: "<i>(Inspirational wisdom)</i>"
     },
     {
       quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
-      author: "Helen Keller"
+      source: "Helen Keller"
     },
     {
       quote: "It is better to be hated for what you are than to be loved for what you are not.",
-      author: "Andre Gide"
+      source: "Andre Gide"
     },
     {
       quote: "I think, therefore I am. - René Descartes",
-      author: "René Descartes",
+      source: "René Descartes",
       citation: "Discourse on the method",
       year: "1637"
     },
     {
       quote: "To be, or not to be, that is the question.",
-      author: "William Shakespeare",
+      source: "William Shakespeare",
       citation: "Hamlet",
       year: "1600"
     },
     {
       quote: "In the end, we will remember not the words of our enemies, but the silence of our friends.",
-      author: "Martin Luther King Jr."
+      source: "Martin Luther King Jr."
     },
     {
       quote: "Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.",
-      author: "Albert Einstein"
+      source: "Albert Einstein"
     },
     {
       quote: "The only thing necessary for the triumph of evil is for good men to do nothing.",
-      author: "Edmund Burke"
+      source: "Edmund Burke"
     }
 ];
 
@@ -163,7 +163,7 @@ function printQuote() {
 
     let quoteObject = getRandomQuote();
     let htmlString = `<p class="quote">${quoteObject.quote}</p>
-                      <p class="source">${quoteObject.author}`
+                      <p class="source">${quoteObject.source}`
 
     if ( quoteObject.citation ) {
       htmlString += `<span class="citation">${quoteObject.citation}</span>`;
